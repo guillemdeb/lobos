8ceaa4277800e885040b79f77c3dc63965e55b8a
dartri <daritrives@gmail.com>
2024-07-04 12:49:09 +0200

    dari v3

--- index.php
+++ index.php
@@ -1,23 +1,39 @@
-<!DOCTYPE html>
-<html lang="es">
-<?php
-include('cab.php');
-?>
-<body>
-    <?php
-    include('adress.php');
-    ?>
-    <header>
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous" />
+        <link href="css/styles.css"rel="stylesheet">
+    </head>
+
+    <body>
         <?php
-        include('header.php');
+        include('preheader.php');
         ?>
+        <header>
+            <!-- place navbar here -->
+        </header>
+        <main></main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
 
-    <section id="inicio" class="hero">
-        <h2>Calidad y Servicio para la Hostelería</h2>
-        <p>Bienvenidos a Higiene Industrial Lobo, su socio de confianza en la provisión de materiales de higiene y limpieza para la industria de la hostelería.</p>
-        <a href="#productos" class="button">Descubra nuestros productos</a>
-    </section>
-
-    <!-- Secciones adicionales como Sobre Nosotros, Productos, Servicios, Blog y Contacto -->
-</body>
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
 </html>
+
